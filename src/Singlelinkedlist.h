ff99278058adac5791ccf04eb19e314e52f8a4f3
Aleksandra Baranska <aleksandra.baranska96@gmail.com>
2018-11-02 14:10:37 +0100

    List files

---
+++ src/Singlelinkedlist.h
@@ -0,0 +1,30 @@
+
+#ifndef SINGLELINKEDLIST_H_
+#define SINGLELINKEDLIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+	int SomeData;
+	struct Node* Next; // Pointer for next element on the list
+}Node;
+
+typedef struct List
+{
+	Node* Head; // Pointer for first element on the list
+
+
+}List;
+
+
+int AddElement (List* element, int addElements);
+void PrintElements ( const List* print);
+int CleanList (List* element);
+List* InitList(void);
+int EraseLast(List* element);
+
+
+
+#endif /* SINGLELINKEDLIST_H_ */
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Singlelinkedlist.h"
+//#include "Singlelinkedlist.c"
+
+
+int main(void)
+{
+	return 0;
+}
